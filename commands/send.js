5c109bdd4585a03190ceacc239fcde5fda7bb1ac
SnakeLuck360GT <56480665+SnakeLuck360GT@users.noreply.github.com>
2021-03-25 22:43:48 +0000

    added from (ep) to (ep) e.g (/send from 20 31)

--- commands/send.js
+++ commands/send.js
@@ -1,18 +1,28 @@
 module.exports = {
-    name: 'send',
-    description: "does a send",
-    execute(message, args, Discord, client){
+  name: 'send',
+  description: "does a send",
+  execute(message, args, Discord, client){
 
-      message.delete()
+    message.delete()
+
+let e2 = args[1]
+    
+    if(args[0] === "from"){
+    while(e2 <= args[2]){
+      message.channel.send("Episode " + e2);
+      e2++;
+    }};
+
+
 
 // originally named cumlord.js
-      let e1 = 1
+    let e1 = 1
 
-      while(e1 <= args[0]){
-        message.channel.send("Episode " + e1);
-        e1++;
-      }
-
-       
+    while(e1 <= args[0]){
+      message.channel.send("Episode " + e1);
+      e1++;
     }
+
+     
+  }
 }
