3309efc70cf5152ebf3f42c1c424cb4941d06830
SnakeLuck360GT <56480665+SnakeLuck360GT@users.noreply.github.com>
2021-03-28 18:55:53 +0100

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -10,7 +10,6 @@ let e2 = args[1]
     if(args[0] === "from"){
     while(e2 <= args[2]){
       message.channel.send("Episode " + e2);
-      message.channel.setTopic(`${args[2]}`);
       e2++;
     }};
 
@@ -21,7 +20,6 @@ let e2 = args[1]
 
     while(e1 <= args[0]){
       message.channel.send("Episode " + e1);
-      message.channel.setTopic(`${args[0]}`);
       e1++;
     }
 
