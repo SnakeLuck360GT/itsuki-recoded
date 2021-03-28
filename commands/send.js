2e974024fd492dacfff7af8be8c68b10e6539875
SnakeLuck360GT <56480665+SnakeLuck360GT@users.noreply.github.com>
2021-03-28 19:06:46 +0100

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -2,21 +2,29 @@ module.exports = {
   name: 'send',
   description: "does a send",
   execute(message, args, Discord, client){
+
     message.delete()
 
-    let e2 = args[1]
+let e2 = args[1]
     
-        if(args[0] === "from"){
-        while(e2 <= args[2]){
-          message.channel.send("Episode " + e2);
-          e2++;
-        }};
+    if(args[0] === "from"){
+    while(e2 <= args[2]){
+      message.channel.setTopic(`${args[2]}`)
+      message.channel.send("Episode " + e2)
+      e2++;
+    }};
 
-        // originally named cumlord.js
+
+
+// originally named cumlord.js
     let e1 = 1
 
     while(e1 <= args[0]){
-      message.channel.send("Episode " + e1);
+      message.channel.setTopic(`${args[0]}`)
+      message.channel.send("Episode " + e1)
       e1++;
-      }
-    }}
+    }
+
+     
+  }
+}
