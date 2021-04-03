293b447df0512238c710d20543b8ed411c3111f8
SnakeLuck360GT <56480665+SnakeLuck360GT@users.noreply.github.com>
2021-04-03 23:46:30 +0100

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,7 +1,7 @@
   module.exports = {
   name: "balance",
   description: "Check the user balance",
-  execute(message, args, client, discord, profileData) {
+  execute(message, args, client, Discord, profileData) {
     message.channel.send(`Your wallet bal is ${profileData.coins}, you banks bal is ${profileData.bank}`);
   },
 };
