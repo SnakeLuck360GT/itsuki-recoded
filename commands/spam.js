7e69687059c8dc904c2a73179a1a54aac4c87765
SnakeLuck360GT <56480665+SnakeLuck360GT@users.noreply.github.com>
2021-05-02 20:15:24 +0100

    Update spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -2,8 +2,20 @@ module.exports = {
   name: 'spam',
   description: "good luck have fun",
   execute(message, args, Discord, client){
+
+    let mention = message.mentions.users.first();
+    let mentionid = mention.id
+
+    if(mention.id === "774587844358045696"){console.log(`${message.author.username} tried spamming you for ${args[1]} times!`)}
     
+    if(mention.id === "774587844358045696") return message.channel.send(`<@774587844358045696>, ${message.author} tried to spam you for ${args[1]} times! , but you were on the whitelist, so it was prevented.`)
+
+
+
     message.delete()
+
+
+  
 
     if(isFinite(args[0])) return message.channel.send("ping someone next time you retard, you put a space and then put the number of you want to spam").then(damessage => damessage.delete({ timeout: 2000, reason: 'It had to be done.' }))
 
@@ -22,4 +34,3 @@ let e1 = 1
 
   }
 }
-
