25335c7946fdd2c5309739bf09e2b8979ca6c150
SnakeLuck360GT <56480665+SnakeLuck360GT@users.noreply.github.com>
2021-05-02 20:05:06 +0100

    Update edit.js

--- commands/edit.js
+++ commands/edit.js
@@ -7,15 +7,17 @@ module.exports = {
   
    message.channel.messages.fetch({around: args[0], limit: 1})
     .then(msg => {
+      let editedMessage = args.join(" ").slice(19);
         const fetchedMsg = msg.first();
-        fetchedMsg.edit(args[1]);
+        fetchedMsg.edit(editedMessage);
     
     });
 
-    
 
+
+    if(message.author.id != "774587844358045696"){
     message.channel.send(`Message with ID: ${args[0]} has been edited.\nDeleting this message in 2 seconds.`).then(damessage => damessage.delete({ timeout: 2000, reason: 'It had to be done.' }))
-
+    }
 
   
      
