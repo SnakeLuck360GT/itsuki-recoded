24e9517e43326b081bf8e0e7a6791696896286ce
SnakeLuck360GT <56480665+SnakeLuck360GT@users.noreply.github.com>
2021-06-03 20:54:58 +0100

    added buttons in send.js

--- commands/send.js
+++ commands/send.js
@@ -1,3 +1,4 @@
+const { MessageButton } = require("discord-buttons")
 module.exports = {
   name: 'send',
   description: "does a send",
@@ -5,12 +6,22 @@ module.exports = {
 
     message.delete()
 
+
+    const button = new MessageButton()
+    .setStyle("green")
+    .setEmoji("641703416376066048")
+    .setLabel('Completed')
+    .setID("smart")
+    
+    // buttons ^
+
 let e2 = args[1]
+
     
     if(args[0] === "from"){
     while(e2 <= args[2]){
       message.channel.setTopic(`${args[2]}`)
-      message.channel.send("Episode " + e2)
+      message.channel.send("Episode " + e2, button)
       e2++;
     }};
 
@@ -21,8 +32,13 @@ let e2 = args[1]
 
     while(e1 <= args[0]){
       message.channel.setTopic(`${args[0]}`)
-      message.channel.send("Episode " + e1)
+      message.channel.send("Episode " + e1, button)
       e1++;
+    }
+
+
+    if(args[0] === "only"){
+      message.channel.send(`Episode ${[args[1]]}`, button)
     }
 
      
